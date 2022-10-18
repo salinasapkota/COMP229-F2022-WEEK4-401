66a5c1178e0d13f841b5fe71627acdec60a3251c
salina sapkota <ssapkota8@my.centennialcollege.ca>
2022-10-17 23:01:31 -0400

    Moongoose Working and Movies Model Completed

--- app/app.js
+++ app/app.js
@@ -9,14 +9,25 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+//Import Mongoose Module
+import mongoose from 'mongoose';
+
 // Configuration Module
-import { Secret } from '../config/config.js';
+import {MongoURI, Secret } from '../config/config.js';
 
 // Import Routes
 import indexRouter from './routes/index.route.server.js'
 
 // Instantiate Express Application
 const app = express();
+
+//complete the DB configuration
+mongoose.connect(MongoURI)
+const db = mongoose.connection;
+
+//Listen for connection success or error
+db.on('open',() => console.log("connected to MongoDB"))
+db.on('error',() => console.log("Mongo Connection Error"))
 
 // Set Up Middlewares
 
