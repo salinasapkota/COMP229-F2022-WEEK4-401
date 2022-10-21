4fe4535a9691ad0ed64c15c5460793cfddb26f33
salina sapkota <ssapkota8@my.centennialcollege.ca>
2022-10-20 22:12:56 -0400

    Added mongo atlas configuration

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
 export const Secret = "MySecret";
-export const MongoURI ="mongodb://127.0.0.1/media"
+//export const MongoURI ="mongodb://127.0.0.1/media"
+export const MongoURI ="mongodb+srv://salinasapkota:salina1526@comp229-f2022-401.dwyrvdp.mongodb.net/media1?retryWrites=true&w=majority"
